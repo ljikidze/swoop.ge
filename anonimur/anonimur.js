9343f1383a09d57f3f3e7783ecf508047167d9f4
unknown <ljikidze@seu.edu.ge>
2025-01-10 23:27:36 +0400

    3 pages remaininig

---
+++ anonimur/anonimur.js
@@ -0,0 +1,8 @@
+document.getElementById('option2').addEventListener('change', function() {
+    document.getElementById('searchBar').style.display = 'block';
+  });
+  
+  document.getElementById('option1').addEventListener('change', function() {
+    document.getElementById('searchBar').style.display = 'none';
+  });
+  
